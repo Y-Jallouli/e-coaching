c9fd005f9e086f7f41c7aeeb969827d7352762e3
Y-Jallouli <61277997+Y-Jallouli@users.noreply.github.com>
2020-04-06 18:43:35 +0100

    Add files via upload

---
+++ js/hoverUsefulLinksFooter.js
@@ -0,0 +1,40 @@
+function dispBlockI1() {
+    var a = document.getElementsByClassName('u1');
+    a[0].classList.remove('d-none');
+
+}
+function dispBlockI2() {
+    var a = document.getElementsByClassName('u2');
+    a[0].classList.remove('d-none');
+}
+
+function dispnone1() {
+    var a = document.getElementsByClassName('u1');
+    a[0].classList.add('d-none');
+}
+
+function dispnone2() {
+    var a = document.getElementsByClassName('u2');
+    a[0].classList.add('d-none');
+
+}
+
+function C1() {
+    var a = document.getElementsByClassName('imgT');
+    a[0].classList.add('opa')
+}
+
+function CD1() {
+    var a = document.getElementsByClassName('imgT');
+    a[0].classList.remove('opa')
+}
+
+function C2() {
+    var a = document.getElementsByClassName('imgT');
+    a[1].classList.add('opa')
+}
+
+function CD2() {
+    var a = document.getElementsByClassName('imgT');
+    a[1].classList.remove('opa')
+}
